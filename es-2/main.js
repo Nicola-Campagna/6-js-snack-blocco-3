5121fced7add78cfd9a6824620e73c2381ba46fa
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-04 12:01:31 +0200

    refactoring logica js snack-2

--- es-2/main.js
+++ es-2/main.js
@@ -2,23 +2,21 @@
 const myArray = [];
 
 // ciclo while
-let i = 0;
 let somma = 0;
 
 while (somma < 50) {
-    const currentNumber = myArray[i];
     // chiedo all'utente un numero da inserire nell'array
-    const number = parseInt(prompt("inserire numero da inserire nell'array"));
+    const number = parseInt(prompt("inserire numero da inserire nell'array < 50"));
     // aggiungo il numero all'array
     myArray.push(number);
-    console.log("numeri array: " + currentNumber);
+    console.log("numeri array: " + myArray);
     // sommo gli elementi 
-    somma += currentNumber;
+    somma += number;
     console.log("somma elementi: " + somma);
-
-    if (somma > 50) {
-        alert("somma elementi > di 50");
-    }
-
-    i++;
 }
+// SE la somma è > di 50 stoppa il giro del ciclo e invia un alert
+if (somma > 50) {
+    alert("STOP! somma elementi > di 50");
+}
+
+console.log(myArray);
